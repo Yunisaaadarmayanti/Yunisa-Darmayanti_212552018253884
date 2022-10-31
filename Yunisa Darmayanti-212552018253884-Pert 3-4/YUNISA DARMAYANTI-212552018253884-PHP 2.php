76d80a9473ce640e347ee95f525e7c36ae405ab5
Yunisaaadarmayanti <114204169+Yunisaaadarmayanti@users.noreply.github.com>
2022-10-31 11:13:26 +0700

    Add files via upload

---
+++ Yunisa Darmayanti-212552018253884-Pert 3-4/YUNISA DARMAYANTI-212552018253884-PHP 2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar php</title>
+</head>
+<body>
+    <?php   
+        echo "<h1>Sekolah Koding</h1>";
+        echo "Halo<br>";
+        echo"Member";
+    ?>
+    Halo Semuanya!
+</body>
+</html>
